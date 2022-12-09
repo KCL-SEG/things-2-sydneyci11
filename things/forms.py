f9bf10cf13811c84189cd0114816862a2cddc871
Chun-I Chien <chun-i.chien@kcl.ac.uk>
2022-12-09 02:22:04 +0000

    First try

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,9 @@
 """Forms of the project."""
-
+from django import forms
+from .models import Thing
 # Create your forms here.
+
+class ThingForm(forms.Form):
+    name = forms.CharField(max_length=35)
+    description = forms.CharField(widget=forms.Textarea(attrs={'rows':3, 'cols': 5}))
+    quantity = forms.IntegerField()
