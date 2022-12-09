c13e418ab345bbac3b15763097933c90681a1fa5
Chun-I Chien <chun-i.chien@kcl.ac.uk>
2022-12-09 02:39:42 +0000

    Third Try

--- things/forms.py
+++ things/forms.py
@@ -3,7 +3,14 @@ from django import forms
 from .models import Thing
 # Create your forms here.
 
-class ThingForm(forms.Form):
-    name = forms.CharField(max_length=35)
+class ThingForm(forms.ModelForm):
+    class Meta:
+        model = Thing
+        fields = ['name', 'description', 'quantity']
+        widgets = {
+            'description': forms.Textarea()
+        }
+
+    """name = forms.CharField(max_length=35)
     description = forms.Textarea(attrs={'rows':3, 'cols': 5})
-    quantity = forms.IntegerField()
+    quantity = forms.IntegerField()"""
